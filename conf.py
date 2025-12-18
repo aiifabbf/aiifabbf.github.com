94f210a418fee4b5942f743dc558b7975a55043b
Benjamin Shi <aiifabbf@outlook.com>
2025-12-18 22:54:32 +0800

    Set default role to literal.

--- conf.py
+++ conf.py
@@ -70,6 +70,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "env"]
 pygments_style = 'vs'
 highlight_language = "none"
 
+default_role = "literal"
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
